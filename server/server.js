88224f40d56049bc4b64fcf881e156e53584e228
Emmanuel Castro <emmanuel.castro@accenture.com>
2020-01-21 17:09:26 -0500

    server changes

--- server/server.js
+++ server/server.js
@@ -1,36 +1,25 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use(require('./routes/usuario'));
 app.get('/', (req, res) => {
     res.json('Hola mundo');
 });
 
-app.get('/usuario', (req, res) => {
-    res.json('get usuario');
-});
 
-app.post('/usuario', (req, res) => {
-    let body =  req.body;
-    if (body.nombre === undefined)
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    else
-        res.json(body);
-});
 
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({id});
-});
-app.delete('/usuario', (req, res) => {
-    res.json('delete usuario');
+mongoose.connect(process.env.URLDB,
+                {useNewUrlParser: true, useCreateIndex: true }, (err,res) => {
+    if (err) throw err;
+
+    console.log('Base da datos global')
 });
 
 app.listen(process.env.PORT, () =>{
